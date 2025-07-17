c135308b683ba0373ae462a4c3b4e5ec0887a195
GianEstrada <giankarlojocsan.estradamartinez@gmail.com>
2025-07-16 20:08:19 -0600

    Fix React Hooks rules violation in admin size systems"

---
+++ server-redirect.js
@@ -0,0 +1,8 @@
+// Treboluxe Backend - Auto-redirect to simple server
+// Este archivo evita el error path-to-regexp usando simple-server.js
+
+console.log('🚀 Starting Treboluxe Backend...');
+console.log('🔄 Using simple-server.js to avoid path-to-regexp errors');
+
+// Importar directamente el servidor simple
+require('./simple-server.js');
