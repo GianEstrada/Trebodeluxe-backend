3fcc71729cc2293af084e736b7db5745c35281d2
GianEstrada <giankarlojocsan.estradamartinez@gmail.com>
2025-07-18 17:02:53 -0600

    Fix: Change bcrypt import to bcryptjs to match package.json dependency

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
 const db = require('../config/db');
 const { generateToken } = require('../utils/tokenUtils');
