1b29cb4a494584c6a8a0ed28858d90fd96538d82
GianEstrada <giankarlojocsan.estradamartinez@gmail.com>
2025-07-23 23:50:09 -0600

    fix: Corregir importación de database en rutas públicas
    - Cambiar require('../config/database') por require('../config/db')
    - Usar destructuring { pool } para coincidir con otros controladores
    - Resolver error 'Cannot find module ../config/database'

--- src/routes/public-index-images.routes.js
+++ src/routes/public-index-images.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../config/database');
+const { pool } = require('../config/db');
 
 // Obtener todas las imágenes del index públicamente (sin autenticación)
 const getPublicIndexImages = async (req, res) => {
